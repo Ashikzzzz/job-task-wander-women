71306f5d499a55fcaafe3202e9d71517e4078f21
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-11-10 21:03:17 +0600

    create all reusable function

---
+++ src/app/route/index.ts
@@ -0,0 +1,9 @@
+// import express from 'express';
+
+// const router = express.Router();
+
+// const moduleRoutes = [];
+
+// moduleRoutes.map(route => router.use(route.path, route.route));
+
+// export default router;
