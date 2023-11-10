5c20f6f6753ae27502b9eaf845765b04ed70d9ef
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-11-10 20:49:05 +0600

    check husky hook

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import { Server } from "http";
-import app from "./app";
-import config from "./config";
+import { Server } from 'http';
+import app from './app';
+import config from './config';
 
-process.on("uncaught exception", (error) => {
+process.on('uncaught exception', error => {
   console.log(error);
   process.exit(1);
 });
@@ -14,17 +14,17 @@ let server: Server;
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("database is connected successfully");
+    console.log('database is connected successfully');
     server = app.listen(config.port, () => {
       console.log(`app is listening in the ${config.port}`);
     });
   } catch (err) {
-    console.log("Failed to connect database", err);
+    console.log('Failed to connect database', err);
   }
 
-  process.on("unhandledRejection", (error) => {
+  process.on('unhandledRejection', error => {
     console.log(
-      "Unhandled rejection is detected we are closing our server....."
+      'Unhandled rejection is detected we are closing our server.....',
     );
     if (server) {
       server.close(() => {
